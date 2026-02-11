c0c6c412a43773b487dc1b43850859fdd196b6dc
ManInBlack-coder <aleksq066@gmail.com>
2026-02-11 12:29:16 +0200

    started implementing db health check functionality

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -1,4 +1,24 @@
-import {mysql} from 'mysql2'
+import { createPool } from 'mysql2';
 
-import {Kysely, MysqlDialect }
-import {database} from '../utils/db'
+import {Kysely, MysqlDialect} from 'kysely'
+import { database } from '../types/databse'
+
+const db = new Kysely<database>({
+    dialect: new MysqlDialect({
+        pool: async () => createPool({
+            host: 'localhost',
+            user: 'root',
+            password: 'qwerty',
+            database: 'noormeister',
+        })
+    })
+})
+export const databaseHealth = () => {
+    if (db) {
+       return true
+    }
+    
+}
+
+
+export default db;
